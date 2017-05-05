72a623a0b7389dcce1a6ba7b3e68cad019f44aa7
vanakkamvenkat <vanakkamvenkat@zoho.com>
2017-05-05 15:50:58 +0530

    Test 2 Pattern 1

---
+++ Test2Pattern1.java
@@ -0,0 +1,18 @@
+package payilagam;
+
+public class Test2Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=1;i<=7;i++)
+		{
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print(j + " "); //Using just 'print' instead of println to cursor in same line while printing
+			}
+			System.out.println();
+		}
+	}
+
+}
