a5052b10dbdc17ccf687d1fd75bcd4244dea3c0b
vanakkamvenkat <vanakkamvenkat@zoho.com>
2017-05-08 15:49:04 +0530

    Test3 Option3

---
+++ Test3Option3.java
@@ -0,0 +1,15 @@
+package payilagam;
+
+public class Test3Option3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String word = "Chennai";
+		System.out.println("The original word to print is " + word);
+		String reverse = new StringBuilder(word).reverse().toString();
+		System.out.println("The reversal of the word is " + reverse );
+
+	}
+
+}
